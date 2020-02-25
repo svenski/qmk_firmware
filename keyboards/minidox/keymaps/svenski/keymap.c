124d7c47c752481cd0d11156ca6902bbc5298a4e
Sergiusz Bleja <sergiusz@bleja.org>
2020-02-25 12:04:57 +0000

    add an escape on hte left thumb

--- keyboards/minidox/keymaps/svenski/keymap.c
+++ keyboards/minidox/keymaps/svenski/keymap.c
@@ -23,7 +23,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 TD(TD_Q_ESC), KC_W, KC_E, KC_R, KC_T,                KC_Y, KC_U, TD(TD_I_STAR), KC_O, KC_P,
 KC_A, MT(MOD_LSFT, KC_S), LCTL_T(KC_D), LT(2,KC_F), KC_G,               TD(TD_H_EQ) , LT(2,KC_J), RCTL_T(KC_K), RSFT_T(KC_L),KC_SCLN,
 LGUI_T(KC_Z), KC_X, KC_C, KC_V, KC_B,                KC_N, KC_M, KC_COMM, KC_DOT, LGUI_T(KC_QUOT),
-             LT(4,KC_NONUS_HASH), ALT_T(KC_TAB), LT(1,KC_ENT),       LT(5,KC_SPC), ALT_T(KC_BSPC), LT(3,KC_SLASH)),
+             LT(4,KC_ESCAPE), ALT_T(KC_TAB), LT(1,KC_ENT),       LT(5,KC_SPC), ALT_T(KC_BSPC), LT(3,KC_SLASH)),
 	[1] = LAYOUT(
 KC_NO, RCTL(KC_W), KC_NO, KC_NO, RCTL(KC_T),                KC_PSLS, KC_7, KC_8, KC_9, KC_PAST,
 KC_NO, RCTL(KC_S), KC_NO, KC_NO, KC_NO,                KC_PEQL, KC_4, KC_5, KC_6, KC_PPLS,
@@ -32,7 +32,7 @@ RCTL(KC_Z), RCTL(KC_X), RCTL(KC_C), RCTL(KC_V), KC_NO,                KC_DOT, KC
 
 	[2] = LAYOUT(
             KC_NO, KC_NO, KC_AMPR, KC_LT, KC_GT,                LSFT(KC_NONUS_HASH), KC_LBRC, KC_RBRACKET, KC_NO, KC_NO,
-            KC_TILD, KC_HASH, KC_LBRC, KC_EXLM, KC_UNDS,                KC_CIRC, KC_LPRN, KC_RPRN, KC_DLR, KC_COLN,
+            KC_NONUS_HASH, KC_HASH, KC_LBRC, KC_UNDS, KC_EXLM,                KC_CIRC, KC_LPRN, KC_RPRN, KC_DLR, KC_COLN,
             KC_NO, KC_NO, KC_NO, KC_SLSH, KC_NONUS_BSLASH,                KC_DQUO, KC_LCBR, KC_RCBR, KC_NO, LSFT(KC_2),
                              KC_NO, KC_BSLS, LSFT(KC_SLSH),               KC_MINS, KC_DELETE, KC_NO),
 	[3] = LAYOUT(KC_F1, KC_F2, KC_F3, KC_F4, KC_F5,       KC_F6, KC_F7, KC_F8, KC_F9, KC_F10,
