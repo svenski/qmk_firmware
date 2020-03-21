c7e171b0904c3872cba8f73a1c24bb80615a733d
Sergiusz Bleja <sergiusz@bleja.org>
2020-03-21 16:34:15 +0000

    Change double tap i to capital i

--- keyboards/minidox/keymaps/svenski/keymap.c
+++ keyboards/minidox/keymaps/svenski/keymap.c
@@ -7,31 +7,31 @@
 enum {
   TD_Q_ESC = 0,
   TD_H_EQ = 1,
-  TD_I_STAR = 2
+  TD_I_CAP = 2
 };
 
 qk_tap_dance_action_t tap_dance_actions[] = {
   //Tap once for Esc, twice for Caps Lock
   [TD_Q_ESC]  = ACTION_TAP_DANCE_DOUBLE(KC_Q, KC_ESC),
   [TD_H_EQ]  = ACTION_TAP_DANCE_DOUBLE(KC_H, KC_EQUAL),
-  [TD_I_STAR]  = ACTION_TAP_DANCE_DOUBLE(KC_I, LSFT(KC_8))
+  [TD_I_CAP]  = ACTION_TAP_DANCE_DOUBLE(KC_I, LSFT(KC_I))
 // Other declarations would go here, separated by commas, if you have them
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[0] = LAYOUT(
-TD(TD_Q_ESC), KC_W, KC_E, KC_R, KC_T,                KC_Y, MT(MOD_LALT, KC_U), TD(TD_I_STAR), KC_O, KC_P,
+TD(TD_Q_ESC), KC_W, KC_E, KC_R, KC_T,                KC_Y, y, TD(TD_I_CAP), KC_O, KC_P,
 MT(MOD_LALT, KC_A), MT(MOD_LSFT, KC_S), LCTL_T(KC_D), LT(2,KC_F), KC_G,               TD(TD_H_EQ) , LT(2,KC_J), RCTL_T(KC_K), RSFT_T(KC_L),KC_COLN,
 LGUI_T(KC_Z), KC_X, KC_C, KC_V, KC_B,                KC_N, KC_M, KC_COMM, KC_DOT, LGUI_T(KC_QUOT),
              ALT_T(KC_ESCAPE), LT(4,KC_TAB), LT(1,KC_ENT),       LT(5,KC_SPC), LT(3,KC_BSPC), ALT_T(KC_SLASH)),
 	[1] = LAYOUT(
-KC_NO, RCTL(KC_W), LCTL(LSFT(KC_LEFT)), LCTL(LSFT(KC_LEFT)), RCTL(KC_T),                KC_7, KC_8, KC_9, KC_PAST, KC_PEQL,
+KC_NO, RCTL(KC_W), LCTL(LSFT(KC_LEFT)), LCTL(LSFT(KC_RGHT)), RCTL(KC_T),                KC_7, KC_8, KC_9, KC_PAST, KC_PEQL,
 RCTL(KC_A), RCTL(KC_S), LCTL(KC_LEFT), LCTL(KC_RGHT), KC_NO,                KC_4, KC_5, KC_6, KC_PPLS, KC_PMNS,
 RCTL(KC_Z), RCTL(KC_X), RCTL(KC_C), RCTL(KC_V), KC_NO,                KC_1, KC_2, KC_3, KC_DOT, KC_PSLS,
              KC_TRNS, KC_NO, KC_NO,               KC_0, KC_BSPC, KC_COMM),
 
 	[2] = LAYOUT(
-            KC_NO, KC_NO, KC_AMPR, KC_LT, KC_GT,                LSFT(KC_NONUS_HASH), KC_LBRC, KC_RBRACKET, KC_NO, KC_NO,
+            KC_NO, KC_NO, KC_AMPR, KC_LT, KC_GT,                LSFT(KC_NONUS_HASH), KC_LBRC, KC_RBRACKET, KC_NO, KC_LBRC,
             KC_NONUS_HASH, KC_HASH, KC_LBRC, KC_UNDS, KC_EXLM,                KC_CIRC, KC_LPRN, KC_RPRN, KC_DLR, KC_SCOLON,
             KC_NO, KC_NO, KC_NO, KC_SLSH, KC_NONUS_BSLASH,                KC_DQUO, KC_LCBR, KC_RCBR, KC_NO, LSFT(KC_2),
                              KC_NO, KC_BSLS, LSFT(KC_SLSH),               KC_MINS, KC_DELETE, KC_NO),
