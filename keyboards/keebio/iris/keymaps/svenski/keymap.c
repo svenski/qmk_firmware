e53f1b0d151d2335eb94632b643b668b00d91d33
Sergiusz Bleja <sergiusz@bleja.org>
2020-10-12 20:54:56 +0100

    Improvements

--- keyboards/keebio/iris/keymaps/svenski/keymap.c
+++ keyboards/keebio/iris/keymaps/svenski/keymap.c
@@ -1,18 +1,32 @@
 #include QMK_KEYBOARD_H
 
+//Tap Dance Declarations
+enum {
+  TD_H_ESQ = 0,
+  TD_Y_EQ = 1,
+  TD_U_CAP = 2,
+};
+
+qk_tap_dance_action_t tap_dance_actions[] = {
+  //Tap once for Esc, twice for Caps Lock
+  [TD_H_ESQ]  = ACTION_TAP_DANCE_DOUBLE(KC_H, KC_ESC),
+  [TD_Y_EQ]  = ACTION_TAP_DANCE_DOUBLE(KC_Y, KC_EQUAL),
+  [TD_U_CAP]  = ACTION_TAP_DANCE_DOUBLE(KC_U, LSFT(KC_8)),
+// Other declarations would go here, separated by commas, if you have them
+};
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [0] = LAYOUT(
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
-     KC_NO,  LSFT(KC_1),    LSFT(KC_2),    LSFT(KC_3),    LSFT(KC_4),    LSFT(KC_5),                               LSFT(KC_6),    LSFT(KC_7),    LSFT(KC_8),    LSFT(KC_9),    LSFT(KC_0),    KC_NO,
+     KC_NO,  LSFT(KC_1),    LSFT(KC_2),    LSFT(KC_3),    LSFT(KC_4),    LSFT(KC_5),                               LSFT(KC_6),    LSFT(KC_7),    KC_NO,    LSFT(KC_9),    LSFT(KC_0),    KC_NO,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,                               KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCOLON,    KC_EQUAL,
+     KC_NO,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,                               KC_J,    KC_L,    TD(TD_U_CAP),    KC_Y,    KC_SCOLON,    KC_NO,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_RGUI, MT(MOD_LALT, KC_A),    MT(MOD_LSFT, KC_R),    LCTL_T(KC_S),    LT(2,KC_T),    KC_D,                               KC_H,    LT(2,KC_N),    RCTL_T(KC_E),    RSFT_T(KC_I),    MT(MOD_LALT, KC_O), KC_COLON,
+     KC_NO, MT(MOD_LALT, KC_A),    MT(MOD_LSFT, KC_R),    LCTL_T(KC_S),    LT(2,KC_T),    KC_D,                               TD(TD_H_ESQ),    LT(2,KC_N),    RCTL_T(KC_E),    RSFT_T(KC_I),    MT(MOD_LALT, KC_O), KC_COLON,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_RGUI, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_SLASH,           KC_ESCAPE,  KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_QUOT, KC_RGUI,
+     KC_NO, LGUI_T(KC_Z),    KC_X,    KC_C,    KC_V,    KC_B,    KC_SLASH,           KC_ESCAPE,  KC_K,    KC_M,    KC_COMM, KC_DOT,  LGUI_T(KC_QUOT), KC_NO,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
                                     ALT_T(KC_ESCAPE), LT(4,KC_TAB),   LT(1,KC_ENT),                    LT(5,KC_SPC),  LT(3,KC_BSPC),   ALT_T(KC_SLASH)
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
@@ -22,17 +36,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
      KC_NO, KC_NO, KC_NO,   KC_NO, KC_NO,  KC_NO,                            KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_PGUP,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_NO,   KC_NO,    RCTL(KC_W),    RCTL(KC_LEFT),    RCTL(KC_RIGHT),    KC_NO,                               LSFT(KC_8),    KC_7,    KC_8,    KC_9,    KC_PEQL,    KC_PGDN,
+     KC_NO,   KC_NO,    RCTL(KC_W),    RCTL(KC_F),    RCTL(KC_P),    RCTL(KC_G),                               LSFT(KC_8),    KC_7,    KC_8,    KC_9,    KC_PEQL,    KC_PGDN,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
      KC_NO,  RCTL(KC_A), RCTL(KC_R), RCTL(KC_S), RCTL(KC_T),   RCTL(KC_D),                            LSFT(KC_EQUAL), KC_4,   KC_5,   KC_6,   KC_MINUS, KC_HOME,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_NO, RCTL(KC_Z), RCTL(KC_X), RCTL(KC_C), RCTL(KC_V), KC_NO, KC_LPRN,          KC_DEL, KC_SLASH, KC_1,   KC_2,   KC_3,   KC_DOT, KC_END,
+     KC_NO, RCTL(KC_Z), RCTL(KC_X), RCTL(KC_C), RCTL(KC_V), RCTL(KC_B), KC_LPRN,          KC_DEL, KC_SLASH, KC_1,   KC_2,   KC_3,   KC_DOT, KC_END,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
                                     _______, _______, KC_NO,                    KC_0,  KC_BSPC, KC_COMM
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   ),
 
- [2] = LAYOUT(KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_AMPR,KC_LT,KC_GT,KC_PERC,LSFT(KC_NONUS_HASH),KC_LBRC,LSFT(KC_EQUAL),KC_RBRACKET,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_GRAVE,KC_HASH,LSFT(KC_SLSH),KC_UNDS,KC_EXLM,KC_CIRC,KC_LPRN,KC_DLR,KC_RPRN,KC_COLON,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,LSFT(KC_NONUS_BSLASH),KC_SLSH,KC_NONUS_BSLASH,KC_LCBR,KC_RCBR,LSFT(KC_8),KC_LCBR,KC_MINS,KC_RCBR,LSFT(KC_2),KC_TRANSPARENT,KC_TRANSPARENT,KC_BSLS,KC_EQUAL,KC_MINS,KC_DELETE,KC_TRANSPARENT),
+ [2] = LAYOUT(KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_ESCAPE,KC_AMPR,KC_LT,KC_GT,KC_PERC,LSFT(KC_NONUS_HASH),KC_LBRC,LSFT(KC_EQUAL),KC_RBRACKET,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_GRAVE,KC_HASH,LSFT(KC_SLSH),KC_UNDS,KC_EXLM,KC_CIRC,KC_LPRN,KC_DLR,KC_RPRN,KC_COLON,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,LSFT(KC_NONUS_BSLASH),KC_SLSH,KC_NONUS_BSLASH,KC_LCBR,KC_RCBR,LSFT(KC_8),KC_LCBR,KC_MINS,KC_RCBR,LSFT(KC_2),KC_TRANSPARENT,KC_TRANSPARENT,KC_BSLS,KC_EQUAL,KC_MINS,KC_DELETE,KC_TRANSPARENT),
 
   [3] = LAYOUT(
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
@@ -47,7 +61,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                     TG(6), _______, LGUI(LALT(KC_BSPACE)),                   _______, _______, _______
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   ),
- [4] = LAYOUT(KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_HOME,KC_PGDN,KC_PGUP,KC_END,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_LEFT,KC_DOWN,KC_UP,KC_RGHT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT),
+ [4] = LAYOUT(KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_HOME,KC_PGDN,KC_PGUP,KC_END,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,LSFT(LALT(KC_BSPACE)),KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_LEFT,KC_DOWN,KC_UP,KC_RGHT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,LCTL(KC_LEFT),LCTL(KC_DOWN),LCTL(KC_UP),LCTL(KC_RGHT),KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT),
  [5] = LAYOUT(KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,LCTL(LALT(KC_LEFT)),LALT(KC_7),LALT(KC_8),LALT(KC_9),LSFT(LCTL(LALT(KC_LEFT))),KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,LALT(KC_H),LALT(KC_4),LALT(KC_5),LALT(KC_6),LALT(KC_L),KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,LCTL(LALT(KC_N)),LALT(KC_1),LALT(KC_2),LALT(KC_3),KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TAB,KC_TRANSPARENT,KC_DEL,KC_TRANSPARENT),
 
   [6] = LAYOUT(
